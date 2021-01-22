cfb7ce91c3d9ef59c06a5705aa13fac835fd1ee1
= <capand369@gmail.com>
2021-01-22 12:29:52 +0900

    hide and seek, add code comments

--- BreadthFirstSearch/1697_hide_and_seek/hide_and_seek.c
+++ BreadthFirstSearch/1697_hide_and_seek/hide_and_seek.c
@@ -49,16 +49,20 @@ void checkChild(int position, int second)
 	usleep(100000);
 #endif
 
+	//결과에 저장된 시간초보다 오래 걸린다면 더이상 계산할 필요가 없음
 	if (struResult.mSecond <= second) {
 		return;
 	}
 
+	//목표 지점보다 현재 위치가 작으면 queue에 push
 	if (position < K) { 
 		pushQueue(&tempStruNode);
 	}
+	//목표 지점에 현재 위치에 도달하면 결과에 저장된 값과 비교
 	else if (position == K) {
 		compareResult(tempStruNode);
 	}
+	//목표 지점보다 현재 위치가 크면 위치의 차이만큼 초를 증가시키고 결과에 저장된 값과 비교
 	else {
 		while (position > 0) {
 			position--;
@@ -79,6 +83,7 @@ void compareResult(SNode inputStruNode)
 	printf("\ncompareResult\n");
 	printf("input p: %d\tinput sec: %d\nresult p: %d\tresult sec: %d\n", inputStruNode.mPosition, inputStruNode.mSecond, struResult.mPosition, struResult.mSecond);
 #endif
+	//결과에 있는 초보다 적은 값이 들어오면 결과 갱신
 	if (struResult.mSecond > inputStruNode.mSecond) {
 		struResult.mPosition = inputStruNode.mPosition;
 		struResult.mSecond = inputStruNode.mSecond;
