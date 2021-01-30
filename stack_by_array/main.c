33e48a69975f1840a6081117e80d64e41cfec237
= <capand369@gmail.com>
2021-01-30 11:06:46 +0900

    stack by array example

--- stack_by_array/main.c
+++ stack_by_array/main.c
@@ -2,26 +2,26 @@
 
 int main(void)
 {
-	st.empty = stackEmpty;
-	st.pop = stackPop;
-	st.push = stackPush;
-	st.size = stackSize;
-	st.top = stackTop;
+	//stack 선언
+	stack *st = (stack *)malloc(sizeof(stack));
 	//stack의 memory 초기화
-	for (int i = 0; i < MAX_SIZE; i++) {
-		st.memory[i].xPos = -1;
-		st.memory[i].yPos = -1;
+	for (int i = 0; i < MAX_STACK_SIZE; i++) {
+		st->memory[i].xPos = -1;
+		st->memory[i].yPos = -1;
 	}
-	st.Top = -1;
+	st->Top = -1;
 
-	char inputString[10];
+	//입력받은 명령어를 저장할 문자열
+	char inputString[MAX_INPUT_SIZE];
+	//push할 data는 여기에 저장해서 인자로 넘겨줌.
 	stackMemNode *nodeData;
+	//push 하기 위해 입력받은 data 임시 저장함.
 	int temp;
 	
 	nodeData = (stackMemNode *)malloc(sizeof(stackMemNode));
 
 	while (1) {
-		printf("1.'empty'\n2.'pop'\n3.'push'\n4.'size'\n5.'top'\n6.'exit'\n");
+		printf("1.'empty'\n2.'pop'\n3.'push'\n4.'size'\n5.'top'\n6.'print'\n7.'exit'\n");
 		printf("input order: ");
 		scanf("%s", inputString); 
 		nodeData->xPos = -1;
@@ -29,18 +29,16 @@ int main(void)
 		
 		//empty 명령이 들어올 때
 		if (!strcmp(inputString, "empty")) {
-			if (st.empty()) {
+			if (empty(st)) {
 				printf("true\n");
 			}
 			else {
 				printf("false\n");
 			}
-			printStack();
 		}
 		//pop 명령이 들어올 때
 		else if (!strcmp(inputString, "pop")) {
-			st.pop();
-			printStack();
+			pop(st);
 		}
 		//push 명령이 들어올 때
 		else if (!strcmp(inputString, "push")) {
@@ -53,20 +51,21 @@ int main(void)
 			scanf("%d", &temp);
 			nodeData->yPos = temp;
 
-			st.push(nodeData);
-			printStack();
+			push(st, nodeData);
 		}
 		//size 명령이 들어올 때
 		else if (!strcmp(inputString, "size")) {
-			printf("%zu\n", st.size());
-			printStack();
+			printf("%zu\n", size(st));
 		}
 		//top 명령이 들어올 때
 		else if (!strcmp(inputString, "top")) {
-			nodeData = st.top();
+			nodeData = top(st);
 
 			printf("xPos: %d, yPos: %d\n", nodeData->xPos, nodeData->yPos);
-			printStack();
+		}
+		//print 명령이 들어올 때
+		else if (!strcmp(inputString, "print")) {
+			printStack(st);
 		}
 		else if (!strcmp(inputString, "exit")) {
 			break;
@@ -74,8 +73,8 @@ int main(void)
 		//그 이외의 명령이 들어올 때
 		else {
 			printf("undefined order\n");
-			printStack();
 		}
+		printf("\n");
 	}
 
 	free(nodeData);
--- stack_by_array/stack_by_array.h
+++ stack_by_array/stack_by_array.h
@@ -3,7 +3,8 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define MAX_SIZE 10
+#define MAX_STACK_SIZE 1000
+#define MAX_INPUT_SIZE 100
 
 //stack에 들어갈 data 구조체
 typedef struct {
@@ -13,22 +14,14 @@ typedef struct {
 
 //stack 구조체
 typedef struct {
-	stackMemNode memory[MAX_SIZE];	//data가 저장될 배열
+	stackMemNode memory[MAX_STACK_SIZE];	//data가 저장될 배열
 	int Top;	//stack의 top 위치를 가리키는 변수
 
-	bool (*empty)(void);
-	void (*pop)(void);
-	void (*push)(stackMemNode *);
-	size_t (*size)(void);
-	stackMemNode *(*top)(void);
-	//void (*emplace)(stackNode);
-	//void (*swap)(stack);
 } stack;
-stack st;
 
-bool stackEmpty(void);
-void stackPop(void);
-void stackPush(stackMemNode *);
-size_t stackSize(void);
-stackMemNode *stackTop(void);
-void printStack(void);
+bool empty(stack *);
+void pop(stack *);
+void push(stack *, stackMemNode *);
+size_t size(stack *);
+stackMemNode *top(stack *);
+void printStack(stack *);
