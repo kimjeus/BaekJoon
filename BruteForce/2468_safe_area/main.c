0541d873143a688df2ea25a1858b32b9ddc54fcc
= <capand369@gmail.com>
2021-02-06 10:42:00 +0900

    safe area modify Comment

--- BruteForce/2468_safe_area/main.c
+++ BruteForce/2468_safe_area/main.c
@@ -17,9 +17,10 @@ int main(void)
 	}
 	resultCount = 1;	//아무 곳도 잠기지 않으면 안전한 영역 1개
 
+	//비오는 양 i
 	for (int i = 1; i < maxHeight; i++) {
-		raining(i);
-		countArea();
+		raining(i);	//비와서 잠긴 영역은 sink 변수에 true 대입하고 앞으로 탐색 안함
+		countArea();	//i만큼 비가 왔을 때 잠기지 않은 안전한 영역의 개수 계산
 	}
 
 	printf("%d\n", resultCount);
--- BruteForce/2468_safe_area/safe_area.h
+++ BruteForce/2468_safe_area/safe_area.h
@@ -13,9 +13,11 @@ Node arrInput[MAX_ROW_SIZE][MAX_ROW_SIZE];
 int N;	//2차원 배열의 행과 열의 개수
 int resultCount;	//안전한 영역의 개수
 
+//비와서 잠긴 영역의 sink변수에 true 대입
+//비오는 양은 증가하기만 하므로 한 번 잠기면 더 이상 그 영역 탐색 안함
 void raining(int);
-void countArea(void);
-void visit(int, int);
+void countArea(void);	//i만큼 비가 왔을 때 잠기지 않은 영역의 개수 계산
+void visit(int, int);	//인자로 입력된 영역과 연결된 모든 잠기지 않은 영역의 방문 체크
 #if DEBUG
 	void printVisit(void);
 	void printSink(void);
